81a2c1b1b5eb98ae3465c7835cb28fd2ccc9ff00
msiam71 <siam.mirza1971@gmail.com>
2026-04-12 23:22:30 +0600

    add post delete option

--- blog/views.py
+++ blog/views.py
@@ -30,3 +30,10 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
