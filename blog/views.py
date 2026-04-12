2fd2d5648065d81dec1c9129716ac57e2efe3cb4
msiam71 <siam.mirza1971@gmail.com>
2026-04-12 21:25:31 +0600

    add edit option in post

--- blog/views.py
+++ blog/views.py
@@ -19,3 +19,14 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_form.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
